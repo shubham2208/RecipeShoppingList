07191552770fb034af568685979f56a1450aa258
shubham2208 <shubham94shah@gmail.com>
2018-04-30 12:54:20 -0700

    Intial Commit

--- README.md
+++ README.md
@@ -1,6 +1,6 @@
 # Recipe
 
-This project was generated with [Angular CLI](https://github.com/angular/angular-cli) version 1.7.3.
+This project was generated with [Angular CLI]version 1.7.3.
 
 ## Development server
 
--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,22 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import * as firebase from 'firebase';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
-  title = 'app';
+export class AppComponent implements OnInit {
+  loadedFeature = 'recipe';
+
+  ngOnInit() {
+    firebase.initializeApp({
+      apiKey: 'AIzaSyAyQ-hP8zhN5XnNysIyQdY3O8j7FI2rPOY',
+      authDomain: 'recipebook-b872f.firebaseapp.com'
+    });
+  }
+
+  onNavigate(feature: string) {
+    this.loadedFeature = feature;
+  }
 }
